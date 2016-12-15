be33924f2e653997cef0670e7b32e382a309ff26
perlancar (@pc-office) <perlancar@gmail.com>
2016-12-15 19:08:02 +0700

    Bump generated prereq version Perinci::CmdLine::{Lite,Base} to 1.66 (observes default dry-run in 'dry_run' feature property in Rinci metadata, Rinci 1.1.83)

--- lib/Perinci/CmdLine/Gen.pm
+++ lib/Perinci/CmdLine/Gen.pm
@@ -386,9 +386,9 @@ sub gen_pericmd_script {
         # determine minimum required version
         if ($cmdline_mod =~ /\APerinci::CmdLine::(Lite|Any)\z/) {
             if ($cmdline_mod eq 'Perinci::CmdLine::Lite') {
-                $cmdline_mod_ver = "1.65";
+                $cmdline_mod_ver = "1.66";
             } else {
-                $extra_modules->{"Perinci::CmdLine::Base"} = "1.65";
+                $extra_modules->{"Perinci::CmdLine::Base"} = "1.66";
             }
         }
 

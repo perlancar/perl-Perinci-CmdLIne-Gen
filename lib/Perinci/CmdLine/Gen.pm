7ffc23fe71b91e612bbb6b0011a652ea57339be3
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-21 14:49:27 +0700

    Bump Perinci::CmdLine::{Lite,Base} version

--- lib/Perinci/CmdLine/Gen.pm
+++ lib/Perinci/CmdLine/Gen.pm
@@ -423,9 +423,9 @@ sub gen_pericmd_script {
         # determine minimum required version
         if ($cmdline_mod =~ /\APerinci::CmdLine::(Lite|Any)\z/) {
             if ($cmdline_mod eq 'Perinci::CmdLine::Lite') {
-                $cmdline_mod_ver = "1.66";
+                $cmdline_mod_ver = "1.72";
             } else {
-                $extra_modules->{"Perinci::CmdLine::Base"} = "1.66";
+                $extra_modules->{"Perinci::CmdLine::Base"} = "1.72";
             }
         }
 

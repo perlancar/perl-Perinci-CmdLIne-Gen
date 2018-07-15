fdbf0a4b14193b62b70c97f17c54cfe5027d38ea
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-07-16 06:24:23 +0700

    Bump generated prereq version: Perinci::CmdLine::{Base,Lite} 1.810 -> 1.811

--- lib/Perinci/CmdLine/Gen.pm
+++ lib/Perinci/CmdLine/Gen.pm
@@ -440,9 +440,9 @@ sub gen_pericmd_script {
         # determine minimum required version
         if ($cmdline_mod =~ /\APerinci::CmdLine::(Lite|Any)\z/) {
             if ($cmdline_mod eq 'Perinci::CmdLine::Lite') {
-                $cmdline_mod_ver = "1.810";
+                $cmdline_mod_ver = "1.811";
             } else {
-                $extra_modules->{"Perinci::CmdLine::Base"} = "1.810";
+                $extra_modules->{"Perinci::CmdLine::Base"} = "1.811";
                 $extra_modules->{"Perinci::CmdLine::Classic"} = "1.770";
             }
         }

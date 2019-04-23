bd972a7021f63201ceb4a8ed928607fadbb04458
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-23 19:06:26 +0700

    [bugfix][regression] Set minimum version for Perinci::CmdLine::Classic

--- lib/Perinci/CmdLine/Gen.pm
+++ lib/Perinci/CmdLine/Gen.pm
@@ -441,9 +441,10 @@ sub gen_pericmd_script {
         if ($cmdline_mod =~ /\APerinci::CmdLine::(Lite|Any)\z/) {
             if ($cmdline_mod eq 'Perinci::CmdLine::Lite') {
                 $cmdline_mod_ver = "1.816";
-            } else {
-                $extra_modules->{"Perinci::CmdLine::Base"} = "1.816";
             }
+        } elsif ($cmdline_mod =~ /\APerinci::CmdLine::Classic\z/) {
+            $extra_modules->{"Perinci::CmdLine::Base"} = "1.816";
+            $extra_modules->{"Perinci::CmdLine::Classic"} = "1.770";
         }
 
         $code = join(
